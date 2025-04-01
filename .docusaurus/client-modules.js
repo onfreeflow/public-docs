2f1858ea30321c1aaaefeef1037fcc10f93df503
freeflow-eric <freeflow-eric@users.noreply.github.com>
2025-04-01 22:54:31 +0000

    deploy: f875e6249340c64694ea555d917b4afad0adecb5

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/erichodonsky/Projects/freeflow/freeflow-public-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/erichodonsky/Projects/freeflow/freeflow-public-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/erichodonsky/Projects/freeflow/freeflow-public-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/erichodonsky/Projects/freeflow/freeflow-public-docs/assets/css/custom.css"),
-];
